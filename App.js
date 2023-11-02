fdda01c53fb1c25a49d763fee42141eb7e9c061e
NilakanthaPradhan <84284559+NilakanthaPradhan@users.noreply.github.com>
2023-11-02 09:11:30 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Image,Button,SafeAreaView,Alert,components} from 'react-native';
+//import { SafeAreaView } from 'react-native-web';
+import { Register } from './Register';
+import React, { useState } from 'react';
 
 export default function App() {
+
+  const [Register, setShowRegister] = useState(false);
+
+  const handleClick = () => {
+    setShowRegister(true);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+  
+    <SafeAreaView style={styles.container}>
+      <View>
+      <Text>Hey welcome Patient  </Text>
+      </View>
+      <View>
+     
+      <Image style={styles.stretch} source= {require('./assets/dlgoo.jpg')} />
       <StatusBar style="auto" />
+      </View>
+      <View>
+      
+         <Button
+          title="Join PearlDoc"
+           onPress={() => Alert.alert('Thank You for Choosing PearlDoc')}
+          />
+
+       
+       
+      
     </View>
+    </SafeAreaView>
   );
 }
-
+const Separator = () => <View style={styles.separator} />;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +44,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  stretch: {
+    width: 450,
+    height: 500,
+    
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  fixToText: {
+    // flexDirection: 'row',
+    // justifyContent: 'space-between',
+  },
 });
+
